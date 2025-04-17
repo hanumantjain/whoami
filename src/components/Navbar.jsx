e7a06440cfa347136503e40094a4f94cf1feef01
hanumantjain <hanumantjain939@gmail.com>
2025-04-17 02:56:42 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { RandomColorContext } from "../components/RandomColor";
+
+const navLinkBase =
+  "text-white px-3 py-2 text-lg font-medium transition-colors hover:[color:var(--primary-color)]";
 
 const Navbar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const primaryColor = useContext(RandomColorContext);
 
-export default Navbar
+  return (
+    <div
+      className="fixed top-6 left-1/2 -translate-x-1/2 z-50 hidden md:block"
+      style={{ "--primary-color": primaryColor }}
+    >
+      <nav className="backdrop-blur-md rounded-full shadow-lg">
+        <div className="px-8 py-4">
+          <div className="flex items-center space-x-8">
+            <Link to="/about" className={navLinkBase}>
+              About Me
+            </Link>
+            <Link to="/experience" className={navLinkBase}>
+              Experience
+            </Link>
+            <Link to="/projects" className={navLinkBase}>
+              Projects
+            </Link>
+            <Link to="/contact" className={navLinkBase}>
+              Contact
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react'
 import {TypeAnimation} from 'react-type-animation'
 import { RandomColorContext } from '../components/RandomColor'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   const primaryColor = useContext(RandomColorContext)
 
   return (
     <div className='min-h-screen flex items-center text-white'>
+      <Navbar />
       <div className=''>
-        <div className='text-6xl pl-30'>
+        <div className='text-3xl md:text-6xl pl-10 md:pl-30'>
           <h1 className='pb-3'>Hey, I'm Hanumant </h1>
             <span>I'm {">"} </span>
             <span style={{ color: primaryColor }}>
